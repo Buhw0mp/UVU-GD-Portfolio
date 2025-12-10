7e740cf6c659c352cb0b70ccbfc5f386f0b0d18a
Kate Nelson <bn@bnchomes.com>
2025-12-10 04:38:43 -0700

    worked on game project some more, ignore its 4 in the morning

--- GameDev1PROJ/Assets/Scripts/Player.cs
+++ GameDev1PROJ/Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -31,8 +35,17 @@ public class Player : MonoBehaviour
         
         inputVector = inputVector.normalized;
 
-        transform.position += (Vector3)inputVector;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
         
-        Debug.Log(inputVector);
+        float rotateSpeed = 10f;
+        
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
